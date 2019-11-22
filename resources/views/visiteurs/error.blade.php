c995bcd0218c3f08ace5c8eed60c43ddf242cc99
rayd <h-123@live.fr>
2019-11-22 01:17:17 +0000

    pre-heroku

---
+++ resources/views/visiteurs/error.blade.php
@@ -0,0 +1,31 @@
+@extends('inc.template')
+
+@section('titre')
+	erreur
+@endsection
+
+@section('header')
+
+@endsection
+
+@section('content')
+<div class="mu-page-breadcrumb-area">
+    <h2 class="pull-left">Error</h2>
+    <ol class="breadcrumb pull-right">
+        <li><a href="{{ url('acceuil') }}"><i class="fa fa-home"></i> Home</a></li>            
+        <li class="active" style="color: gold;">Une erreur s'est produite</li>
+    </ol>
+</div>
+@endsection
+
+@section('layout_main_content')
+<div class="container">
+	<div class="row">
+		<h3>{{ $message }}</h3>
+	</div>
+</div>
+@endsection
+
+@section('footer')
+
+@endsection
