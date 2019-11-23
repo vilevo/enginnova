d1a7477b98e7d548770a6c6ee604738db819a683
rayd <h-123@live.fr>
2019-11-23 17:40:08 +0000

    modification sql

--- enseed.sql
+++ enseed.sql
@@ -1,36 +1,7 @@
-INSERT INTO booster_forfaits (name) 
-VALUES ('une semaine'),
-	('deux semaines');
-
-INSERT INTO categories (name) 
-VALUES ('developpement web'),
-('developpement mobil'),
-('maths'),
-('genie civil'),
-('graphisme'),
-('devops'),
-('chimie'),
-('physique'),
-('bureautique'),
-('finance');
 
 INSERT INTO observations (name,valeur) 
 VALUES ('super bon', 10),
-('bon', 9,),
-('assez bien', 7,),
-('passable', 5,),
-('insuffisent', 3,);
-
-INSERT INTO professions (name) 
-VALUES ('etudiant'),
-('entrepreneur');
-
-INSERT INTO gestion_activites (titre,contenu,photo) 
-VALUES ('test numéro 1', '<p>Vivre l&rsquo;exp&eacute;rience start-up ! Le cursus et l&rsquo;environnement de Enginnova offrent des opportunit&eacute;s de rencontres et d&rsquo;&eacute;changes qui permettent d&rsquo;&eacute;largir son r&eacute;seau, de stimuler la cr&eacute;ativit&eacute; et l&rsquo;innovation.</p>', '1572271845.jpeg'),
-('teste numéro 2', '<p>Vivre l&rsquo;exp&eacute;rience start-up ! Le cursus et l&rsquo;environnement de Enginnova offrent des opportunit&eacute;s de rencontres et d&rsquo;&eacute;changes qui permettent d&rsquo;&eacute;largir son r&eacute;seau, de stimuler la cr&eacute;ativit&eacute; et l&rsquo;innovation.</p>', '1573115226.jpeg'),
-('test numéro 3', '<p>Vivre l&rsquo;exp&eacute;rience start-up ! Le cursus et l&rsquo;environnement de Enginnova offrent des opportunit&eacute;s de rencontres et d&rsquo;&eacute;changes qui permettent d&rsquo;&eacute;largir son r&eacute;seau, de stimuler la cr&eacute;ativit&eacute; et l&rsquo;innovation.</p>', '1573115275.jpeg'),
-('test numéro 4', '<p>Vivre l&rsquo;exp&eacute;rience start-up ! Le cursus et l&rsquo;environnement de Enginnova offrent des opportunit&eacute;s de rencontres et d&rsquo;&eacute;changes qui permettent d&rsquo;&eacute;largir son r&eacute;seau, de stimuler la cr&eacute;ativit&eacute; et l&rsquo;innovation.</p>', '1573115322.jpeg'),
-('test numéro 5', '<p>Vivre l&rsquo;exp&eacute;rience start-up ! Le cursus et l&rsquo;environnement de Enginnova offrent des opportunit&eacute;s de rencontres et d&rsquo;&eacute;changes qui permettent d&rsquo;&eacute;largir son r&eacute;seau, de stimuler la cr&eacute;ativit&eacute; et l&rsquo;innovation.</p>', '1573115351.jpeg');
-
-INSERT INTO astuces (titre,contenu,photo) 
-VALUES ('astuce num 1', '<p>D&eacute;taill&eacute; l&#39;astuce ici...&nbsp;D&eacute;taill&eacute; l&#39;astuce ici&nbsp;D&eacute;taill&eacute; l&#39;astuce ici&nbsp;D&eacute;taill&eacute; l&#39;astuce ici</p><p>D&eacute;taill&eacute; l&#39;astuce ici&nbsp;D&eacute;taill&eacute; l&#39;astuce ici&nbsp;D&eacute;taill&eacute; l&#39;astuce ici&nbsp;D&eacute;taill&eacute; l&#39;astuce ici...</p>', '1572780080.jpg');
+('bon', 9),
+('assez bien', 7),
+('passable', 5),
+('insuffisent', 3);
