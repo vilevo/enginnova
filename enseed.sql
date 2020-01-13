dd1483f3242ce9ade24a7844188167f36d72de3a
rayd <h-123@live.fr>
2020-01-13 20:25:51 +0000

    changes

--- enseed.sql
+++ enseed.sql
@@ -1,4 +1,14 @@
 UPDATE gestion_slides
-SET image = "slider1.jpg",
+SET image = "slider1.jpg"
 WHERE
    id=1;
+
+UPDATE gestion_slides
+SET image = "slider2.jpg"
+WHERE
+   id=2;
+
+UPDATE gestion_slides
+SET image = "slider3.jpg"
+WHERE
+   id=3;
