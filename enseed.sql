182f3ecb248fda485ab3765d6d420c5fc99b6b93
rayd <h-123@live.fr>
2020-01-14 14:17:45 +0000

    yeah

--- enseed.sql
+++ enseed.sql
@@ -1,14 +1,2 @@
-UPDATE gestion_slides
-SET image = 'slider1.jpg'
-WHERE
-   id=1;
-
-UPDATE gestion_slides
-SET image = 'slider2.jpg'
-WHERE
-   id=2;
-
-UPDATE gestion_slides
-SET image = 'slider3.jpg'
-WHERE
-   id=3;
+ALTER TABLE freelance_projets
+	ADD COLUMN type VARCHAR;
