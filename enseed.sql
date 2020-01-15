1128ae03b3233afb8a95302082350f4d3e1dde3d
rayd <h-123@live.fr>
2020-01-15 19:35:49 +0000

    yeah

--- enseed.sql
+++ enseed.sql
@@ -1,6 +1,2 @@
-INSERT INTO roles (id,name)
-	VALUES ('1','user'),
-		  ('2','admin');
-
-INSERT INTO role_user (id,name)
-	VALUES ('2','1');
+INSERT INTO role_user (role_id,user_id)
+	VALUES ('1','1');
