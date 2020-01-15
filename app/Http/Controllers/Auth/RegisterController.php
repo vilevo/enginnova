8da49d8bdd72e4f533f20e62089803f5493e95f9
rayd <h-123@live.fr>
2020-01-15 20:07:00 +0000

    yeah

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = 'VerifyEmailFirst';
+    protected $redirectTo = 'user/home';
 
     /**
      * Create a new controller instance.
